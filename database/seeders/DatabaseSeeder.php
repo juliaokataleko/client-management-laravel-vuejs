2ec4a6e604f8f82e66fc1a71ca16e2c49991f59a
juliaokataleko <juliofeli78@gmail.com>
2022-01-14 22:06:25 +0000

    Primeiro commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('admin'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
